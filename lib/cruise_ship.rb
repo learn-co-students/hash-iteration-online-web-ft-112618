f1f3c3e249e65bf283d4872e8cd65cf5d9a5dc51
Don Ward <10is0420@gmail.com>
2018-12-10 04:40:24 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,13 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      passengers = name 
+    end
+end
+
+passengers
+
 end
